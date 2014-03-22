8194a6aa8f3b6609cddca57c2422c10c3795e740
azte <cuellar.uk@gmail.com>
2014-03-21 22:35:29 -0600

    Reportes Asociados al ID de usuario
    A cada reporte se le asigna el id_u automaticamente desde la variable
    $SESSION['usuarioactual]
    
    ¡ESTUDIAR COMO FUCKING VERGAS FUNCIONAN LOS FETCHARRAY!

--- genera_r.php
+++ genera_r.php
@@ -1,6 +1,3 @@
-
-
-
 <?php
 
 //Esta linea es para incluir el archivo con las variables
@@ -12,8 +9,17 @@ or die('No se pudo conectar: ' . mysql_error());
 
 mysql_select_db('prueba') or die('No se pudo seleccionar la base de datos'); 
 
+
+session_start();
+
+
+$consulta="SELECT idu from usuarios where nombre='$_SESSION[usuarioactual]'";
+$idr=mysql_query($consulta);
+$resEmp = mysql_fetch_assoc($idr);
+   //echo $resEmp['idu']."<br>";
+
 $fecha=date("y/m/d");
-$query = "INSERT INTO reportes(usuario,tienda,comentarios,tipo,subtipo,fecha) values('$_POST[usuario]','$_POST[tienda]',
+$query = "INSERT INTO reportes(id_u,usuario,tienda,comentarios,tipo,subtipo,fecha) values('$resEmp[idu]','$_POST[usuario]','$_POST[tienda]',
 	'$_POST[comentarios]','$_POST[tipo]','$_POST[subtipo]','$fecha')";
 $result = mysql_query($query);
 
@@ -31,10 +37,12 @@ exit();
 
 ?>
 
-<script type="text/javascript">
-
+	<script type="text/javascript">
 	
-alert("REPORTE REGISTRADO CORRECTAMENTE")
-window.location="http://localhost/call-manager/llamadas.php"
+		
+	alert("REPORTE REGISTRADO CORRECTAMENTE")
+	window.location="http://localhost/call-manager/llamadas.php"
+	
+	</script>
 
-</script>
+
